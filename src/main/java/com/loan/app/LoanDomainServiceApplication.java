38aac49e2e9a52692f48a52287d02a1a376d6a77
LimJiNa <jn4624@gmail.com>
2024-01-18 14:06:57 +0900

    대출 상담 수정 기능 구현

--- src/main/java/com/loan/app/LoanDomainServiceApplication.java
+++ src/main/java/com/loan/app/LoanDomainServiceApplication.java
@@ -2,7 +2,9 @@ package com.loan.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class LoanDomainServiceApplication {
 
--- src/test/java/com/loan/app/service/CounselServiceTest.java
+++ src/test/java/com/loan/app/service/CounselServiceTest.java
@@ -84,4 +84,26 @@ public class CounselServiceTest {
 
         Assertions.assertThrows(BaseException.class, () -> counselService.get(findId));
     }
+
+    @Test
+    void Should_ReturnUpdatedResponseOfExistCounselEntity_When_RequestUpdateExistCounselInfo() {
+        Long findId = 1L;
+
+        Counsel entity = Counsel.builder()
+                .counselId(1L)
+                .name("Member Lim")
+                .build();
+
+        Request request = Request.builder()
+                .name("Member Kim")
+                .build();
+
+        when(counselRepository.save(any(Counsel.class))).thenReturn(entity);
+        when(counselRepository.findById(findId)).thenReturn(Optional.ofNullable(entity));
+
+        Response actual = counselService.update(findId, request);
+
+        assertThat(actual.getCounselId()).isSameAs(findId);
+        assertThat(actual.getName()).isSameAs(request.getName());
+    }
 }
