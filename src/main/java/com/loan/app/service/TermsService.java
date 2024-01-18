bd77405736b15b2f7292927eaaeab397470ff198
LimJiNa <jn4624@gmail.com>
2024-01-18 22:56:56 +0900

    이용 약관 조회 기능 구현

--- src/main/java/com/loan/app/service/TermsService.java
+++ src/main/java/com/loan/app/service/TermsService.java
@@ -3,6 +3,10 @@ package com.loan.app.service;
 import com.loan.app.dto.TermsDTO.Response;
 import com.loan.app.dto.TermsDTO.Request;
 
+import java.util.List;
+
 public interface TermsService {
     Response create(Request request);
+
+    List<Response> getAll();
 }
--- src/main/java/com/loan/app/service/TermsServiceImpl.java
+++ src/main/java/com/loan/app/service/TermsServiceImpl.java
@@ -8,6 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 @RequiredArgsConstructor
 public class TermsServiceImpl implements TermsService {
@@ -21,5 +24,11 @@ public class TermsServiceImpl implements TermsService {
         Terms created = termsRepository.save(terms);
 
         return modelMapper.map(created, Response.class);
+    }
+
+    @Override
+    public List<Response> getAll() {
+        List<Terms> termsList = termsRepository.findAll();
+        return termsList.stream().map(t -> modelMapper.map(t, Response.class)).collect(Collectors.toList());
     }
 }
